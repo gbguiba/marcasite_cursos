35d5dfd5a1f863f780ecf6a3f1ba98a7ae387023
Guilherme Barros <215538752+gbguiba@users.noreply.github.com>
2025-11-22 12:00:17 -0300

    Criação da tabela "users" e de seu respectivo modelo de acesso. Essa tabela armazenará os dados dos usuários.

---
+++ database/migrations/2025_11_22_115059_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+
+return new class extends Migration {
+
+    public function up(): void {
+
+        Schema::create('users', function (Blueprint $table) {
+
+            $table->uuid('id')->primary();
+
+            $table->timestamps();
+
+            $table->string('ip');
+            $table->text('user_agent');
+
+            $table->string('name');
+            $table->enum('type', ['user', 'admin'])->default('user');
+            $table->string('email');
+            $table->string('password');
+            $table->string('photo_path')->nullable();
+
+        });
+
+    }
+
+    public function down(): void {
+
+        Schema::dropIfExists('users');
+
+    }
+
+};
