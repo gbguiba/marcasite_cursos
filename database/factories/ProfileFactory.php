773f63fb28bf989e501d7b8b7e82e8146e799ab0
Guilherme Barros <215538752+gbguiba@users.noreply.github.com>
2025-11-24 13:26:06 -0300

    Criação da fábrica do model "Profile".

---
+++ database/factories/ProfileFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\User;
+
+class ProfileFactory extends Factory {
+
+    public function definition(): array {
+
+        return [
+            'id' => (string) Str::uuid(),
+            'ip' => $this->faker->ipv4(),
+            'user_agent' => $this->faker->userAgent(),
+            'user_id' => User::factory(),
+            'name' => $this->faker->name(),
+            'photo_path' => null,
+            'cpf' => $this->faker->numerify('###########'),
+        ];
+
+    }
+
+}
