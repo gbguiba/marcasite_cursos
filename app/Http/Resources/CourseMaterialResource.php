2d5bcf0d4657f0003facb3198298c8e2e0828851
Guilherme Barros <215538752+gbguiba@users.noreply.github.com>
2025-11-26 19:54:42 -0300

    Listagem dos respectivos materiais de um curso, quando um curso ou uma lista de cursos é exibida.

---
+++ app/Http/Resources/CourseMaterialResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\CourseResource;
+
+class CourseMaterialResource extends JsonResource {
+    
+    public function toArray(Request $request): array {
+
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'path' => $this->path,
+            'course' => new CourseResource($this->course),
+        ];
+
+    }
+
+}
--- app/Http/Resources/CourseResource.php
+++ app/Http/Resources/CourseResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Http\Resources\CourseCategoryResource;
+use App\Http\Resources\CourseMaterialResource;
 
 class CourseResource extends JsonResource {
     
@@ -14,7 +15,7 @@ class CourseResource extends JsonResource {
             'id' => $this->id,
             'created_at' => $this->created_at,
             'name' => $this->name,
-            'courseCategory' => new CourseCategoryResource($this->courseCategory),
+            'courseCategory' => new CourseCategoryResource($this->whenLoaded('courseCategory')),
             'price' => $this->price,
             'places' => $this->places,
             'registrationStart' => $this->registration_start,
@@ -22,6 +23,7 @@ class CourseResource extends JsonResource {
             'description' => $this->description,
             'thumbnail' => $this->thumbnail,
             'active' => (bool) $this->active,
+            'materials' => CourseMaterialResource::collection($this->whenLoaded('courseMaterial')),
         ];
 
     }
